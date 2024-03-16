565176b649b264fb16888f73434514a19f862b42
Naoki Iida <niida@student.42tokyo.jp>
2024-03-16 15:02:03 +0900

    feat: test script

--- test.sh
+++ test.sh
@@ -1,2 +1,28 @@
 #!/bin/bash
-DYLD_LIBRARY_PATH=/Users/niida/Documents/42cursus/minishell/ext/lib/ ./minishell
+#DYLD_LIBRARY_PATH=/Users/niida/Documents/42cursus/minishell/ext/lib/ ./minishell
+export DYLD_LIBRARY_PATH=/Users/niida/Documents/42cursus/minishell/ext/lib/
+assert_bash() {
+   # テストしようとしている内容をprint
+	printf '%-30s:' "\"$1\""
+
+	# bashの出力をcmpに保存
+	echo -n -e "$1" | bash >cmp 2>&-
+	# bashのexit statusをexpectedに代入
+	expected=$?
+	# minishellの出力をoutに保存
+	echo -n -e "$1" | ./minishell >out 2>&-
+	# minishellのexit statusをactualに代入
+	actual=$?
+
+	# bashとminishellの出力を比較
+	diff cmp out >/dev/null && echo -n '  diff OK' || echo -n '  diff NG'
+
+	# bashとminishellのexit statusを比較
+	if [ "$actual" = "$expected" ]; then
+		echo -n '  status OK'
+	else
+		echo -n "  status NG, expected $expected but got $actual"
+	fi
+	echo
+}
+assert_bash ''
